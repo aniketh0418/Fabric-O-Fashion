27a2ff8fc9449dba0f92482c8f38869d601838af
Aniketh <135521872+aniketh0418@users.noreply.github.com>
2024-11-24 08:06:44 +0530

    Update ProjectAssistant.py

--- components/pages/ProjectAssistant.py
+++ components/pages/ProjectAssistant.py
@@ -3,7 +3,7 @@ import streamlit as st
 from data.fabric_data import fabric_df
 from PIL import Image
 
-img = Image.open(r"Fabric-O-Fashion\\components\\pages\\logowhite.png")
+img = Image.open(r"\components\pages\logowhite.png")
 
 def show_project_assistant():
     st.image(img, width=100, use_column_width=False)
